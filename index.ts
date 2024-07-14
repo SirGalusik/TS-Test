9d7cbd4e7390e7ad82429c5a9d1bcd4bbfe7a18b
MrArtga <artga03@gmail.com>
2024-07-14 11:55:24 +0300

    TS

---
+++ index.ts
@@ -0,0 +1,31 @@
+// integer
+
+let n : number = 4;
+
+console.log(n);
+
+const n1 : number = 5;
+console.log(n1);
+
+// string
+
+const s1 : string = 'hello';
+const s2 : string = ' TS';
+console.log(s1 + s2) 
+
+// boolean
+
+const b1 : boolean = true;
+console.log(b1, typeof b1);
+
+// Преобразование 
+
+const c1 : number = Number('333');
+console.log(c1);
+
+const c2 : string = String(333);
+console.log(c2);
+
+const c3 : boolean = Boolean(1);
+const c4 : boolean = Boolean(0);
+console.log(c3, c4);
---
+++ javascript/index.js
@@ -0,0 +1,22 @@
+"use strict";
+// integer
+let n = 4;
+console.log(n);
+const n1 = 5;
+console.log(n1);
+// string
+const s1 = 'hello';
+const s2 = ' TS';
+console.log(s1 + s2);
+// boolean
+const b1 = true;
+console.log(b1, typeof b1);
+// Преобразование 
+const c1 = Number('333');
+console.log(c1);
+const c2 = String(333);
+console.log(c2);
+const c3 = Boolean(1);
+const c4 = Boolean(0);
+console.log(c3, c4);
+//# sourceMappingURL=index.js.map
